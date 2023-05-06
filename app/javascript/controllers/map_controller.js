320602f0cd2eb5f2d3349b752e9768865622803f
Murilo Gomes <mgfreires@hotmail.com>
2023-05-06 17:44:55 +0200

    Displaying Map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
@@ -7,11 +8,41 @@ export default class extends Controller {
   }
 
   connect() {
+    console.log("Hello from the map controller 🫣");
     mapboxgl.accessToken = this.apiKeyValue
-
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/jlgrobe/cjnpu2gu108yq2rpesfnrqc4g"
     })
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+
+    this.map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+      })
+    );
+  }
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window_html);
+
+      const customMarker = document.createElement("div");
+      customMarker.innerHTML = marker.marker_html;
+
+      new mapboxgl.Marker(customMarker)
+        .setPopup(popup)
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(this.map);
+    });
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds();
+    this.markersValue.forEach((marker) =>
+      bounds.extend([marker.lng, marker.lat])
+    );
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
   }
 }
