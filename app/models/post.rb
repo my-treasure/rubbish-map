f9e5f727ad7869df81b934a7dc07b9b6fe8b181c
Murilo Gomes <mgfreires@hotmail.com>
2023-05-11 21:38:09 +0200

    changes posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_one_attached :post_image
 
 end
--- db/seeds/seeds.rb
+++ db/seeds/seeds.rb
@@ -115,17 +115,22 @@ n_posts.times do
   rand_latitude = rand(52.4901..52.5130)
   rand_longitude = rand(13.3888..13.4449)
   reverse_geocode = Geocoder.search([rand_latitude, rand_longitude])
+  photo_blob = ALL_IMAGES["resources"].sample
+  file = URI.open(photo_blob["url"])
 
-  Post.create(
+  new_post = Post.create(
     title: Faker::Lorem.sentence(word_count: 3),
     body: Faker::Lorem.paragraph(sentence_count: 2),
     user_id: User.all.sample.id,
-    post_image: ALL_IMAGES["resources"].sample["secure_url"],
+
     address: reverse_geocode.first.address,
     latitude: rand_latitude,
     longitude: rand_longitude,
     created_at: Faker::Date.between(from: '2022-01-01', to: '2023-05-10')
   )
+
+  new_post.post_image.attach(io: file, filename: photo_blob["public_id"] , content_type: "image/png")
+  new_post.save
   puts "Created post"
 end
 
