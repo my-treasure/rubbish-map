f77aef9ad9fbf46d420107ff9ff838bb03c09e56
Diego Papera <papera.diego@gmail.com>
2023-05-11 19:26:05 +0200

    started working on the likes button

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   #mount_uploader :post_image, PostImageUploader
