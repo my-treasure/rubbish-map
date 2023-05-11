43c32d18d0cbaa485dcd1381cde76a10a2e25a9b
Murilo Gomes <mgfreires@hotmail.com>
2023-05-11 07:16:44 +0200

    changes on post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,6 @@ class Post < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   #mount_uploader :post_image, PostImageUploader
+  # gem carrierwave
 
 end
